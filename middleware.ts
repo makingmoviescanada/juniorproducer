f0a17a8e8d25f276c2037c39ccaf7a03490c983c
makingmoviescanada <gavin.seal@intersectionnelfilms.com>
2026-04-01 07:13:23 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(['/chat(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  if (isProtectedRoute(req)) await auth.protect({ unauthenticatedUrl: new URL('/login', req.url).toString() })
 })
 
 export const config = {
